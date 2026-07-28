f3a29d1f5a8d3094ca9ee89655028f4762b44b82
Smart Dash <neoalmasana@gmail.com>
2026-07-28 20:56:40 -0300

    feat(knowledge): add native business knowledge package

---
+++ pymia/business_knowledge/__init__.py
@@ -0,0 +1,5 @@
+"""Versioned operational knowledge packs for PymIA.
+
+Knowledge in this package is non-authorizing by default. Productive execution
+requires explicit promotion through the Service 1 architecture controls.
+"""
